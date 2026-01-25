23801c66fc288a6ee6ed361b28c8954de167cc74
sixzenith-marvel <marvel@sixzenith.com>
2026-01-25 21:41:37 +0700

    mail

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || 're_123');
 
 export async function sendPasswordResetEmail(email: string, token: string) {
     const resetLink = `${process.env.NEXTAUTH_URL}/reset-password?token=${token}`;
